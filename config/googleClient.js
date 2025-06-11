c5b3e007ccd52f385a90d0d727f6c23fa00eede5
ERodbot <emarodriguez@estudiantec.cr>
2025-06-10 22:35:52 -0700

    testing

--- config/googleClient.js
+++ config/googleClient.js
@@ -6,6 +6,5 @@ console.log(process.env.GOOGLE_CLIENT_ID);
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI,
-  { disablePKCE: true }
+  process.env.GOOGLE_REDIRECT_URI
 );
